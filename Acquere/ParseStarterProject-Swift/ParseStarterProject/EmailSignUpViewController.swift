18d431b0871cef9b30dee781765cf77255729b4a
RAHUL SHRESTHA <rahul.shrestha@selu.edu>
2015-09-19 01:26:07 -0500

    alright alright alright.... changes to the email sign up! The UX is minimal and uniform. Need to add legal. and the parse sign up!

--- Acquere/ParseStarterProject-Swift/ParseStarterProject/EmailSignUpViewController.swift
+++ Acquere/ParseStarterProject-Swift/ParseStarterProject/EmailSignUpViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 
 class EmailSignUpViewController: UIViewController {
+    @IBOutlet weak var signUpButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+         decorateButton(signUpButton, color: UIColor(red: 0.114, green: 0.914, blue: 0.714, alpha: 1))
         
+    }
+    
+    private func decorateButton(button: UIButton, color: UIColor) {
         
+        button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        button.layer.borderColor = color.CGColor
+        button.backgroundColor = color
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 7
     }
     
 }
