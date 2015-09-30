93b068bd8baf35fa3c7c8209ad2e1aeebd1aa0da
RAHUL SHRESTHA <rahul.shrestha@selu.edu>
2015-09-30 13:47:16 -0500

    working around with Facebook sdks for 9 but lots of issues. updating the bucket before OS X update.

---
+++ Acquere Swift 2.0 FBSDK Error/ParseStarterProject-Swift/ParseStarterProject/AppDelegate.swift
@@ -0,0 +1,164 @@
+/**
+* Copyright (c) 2015-present, Parse, LLC.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+import UIKit
+import Fabric
+import TwitterKit
+import FBSDKCoreKit
+import ParseFacebookUtilsV4
+import Parse
+
+// If you want to use any of the UI components, uncomment this line
+// import ParseUI
+
+// If you want to use Crash Reporting - uncomment this line
+// import ParseCrashReporting
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    //--------------------------------------
+    // MARK: - UIApplicationDelegate
+    //--------------------------------------
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0.31, green: 0.765, blue: 0.969, alpha: 1.0)
+        //UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        UINavigationBar.appearance().backIndicatorImage = UIImage(named: "GoBack")!
+        let backImg: UIImage = UIImage(named: "GoBack")!
+        UIBarButtonItem.appearance().setBackButtonBackgroundImage(backImg, forState: .Normal, barMetrics: .Default)
+      
+        
+        // Enable storing and querying data from Local Datastore.
+        // Remove this line if you don't want to use Local Datastore features or want to use cachePolicy.
+        Parse.enableLocalDatastore()
+        Fabric.with([Twitter.self()])
+
+        // ****************************************************************************
+        // Uncomment this line if you want to enable Crash Reporting
+        // ParseCrashReporting.enable()
+        //
+        // Uncomment and fill in with your Parse credentials:
+        
+        Parse.setApplicationId("0WIFgvDjclrIRUZJTD5MjW02iNfv74GkUD78ipC6",
+            clientKey: "mbJQmq5YAE21XLruBib7HDPV5VAsfOlQY1EV3tHk")
+        
+     
+        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
+        // ****************************************************************************
+        
+        PFUser.enableAutomaticUser()
+
+        let defaultACL = PFACL();
+
+        // If you would like all objects to be private by default, remove this line.
+        defaultACL.setPublicReadAccess(true)
+
+        PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser:true)
+
+        if application.applicationState != UIApplicationState.Background {
+            // Track an app open here if we launch with a push, unless
+            // "content_available" was used to trigger a background push (introduced in iOS 7).
+            // In that case, we skip tracking here to avoid double counting the app-open.
+
+            let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
+            let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
+            var noPushPayload = false;
+            if let options = launchOptions {
+                noPushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil;
+            }
+            if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
+                PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+            }
+        }
+        //
+        //  Swift 2.0
+        //
+        //        if #available(iOS 8.0, *) {
+        //            let types: UIUserNotificationType = [.Alert, .Badge, .Sound]
+        //            let settings = UIUserNotificationSettings(forTypes: types, categories: nil)
+        //            application.registerUserNotificationSettings(settings)
+        //            application.registerForRemoteNotifications()
+        //        } else {
+        //            let types: UIRemoteNotificationType = [.Alert, .Badge, .Sound]
+        //            application.registerForRemoteNotificationTypes(types)
+        //        }
+        
+        
+    
+        return FBSDKApplicationDelegate.sharedInstance().application(application,didFinishLaunchingWithOptions : launchOptions)
+    }
+
+    //--------------------------------------
+    // MARK: Push Notifications
+    //--------------------------------------
+
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        let installation = PFInstallation.currentInstallation()
+        installation.setDeviceTokenFromData(deviceToken)
+        installation.saveInBackground()
+
+        PFPush.subscribeToChannelInBackground("") { (succeeded: Bool, error: NSError?) in
+            if succeeded {
+                print("ParseStarterProject successfully subscribed to push notifications on the broadcast channel.");
+            } else {
+                print("ParseStarterProject failed to subscribe to push notifications on the broadcast channel with error = %@.", error)
+            }
+        }
+    }
+
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        if error.code == 3010 {
+            print("Push notifications are not supported in the iOS Simulator.")
+        } else {
+            print("application:didFailToRegisterForRemoteNotificationsWithError: %@", error)
+        }
+    }
+
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        PFPush.handlePush(userInfo)
+        if application.applicationState == UIApplicationState.Inactive {
+            PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+        }
+    }
+
+    ///////////////////////////////////////////////////////////
+    // Uncomment this method if you want to use Push Notifications with Background App Refresh
+    ///////////////////////////////////////////////////////////
+    // func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+    //     if application.applicationState == UIApplicationState.Inactive {
+    //         PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+    //     }
+    // }
+
+    //--------------------------------------
+    // MARK: Facebook SDK Integration
+    //--------------------------------------
+    func application(application: UIApplication,
+        openURL url: NSURL,
+        sourceApplication: String?,
+        annotation: AnyObject?) -> Bool {
+            return FBSDKApplicationDelegate.sharedInstance().application(application,
+                openURL: url,
+                sourceApplication: sourceApplication,
+                annotation: annotation)
+    }
+    
+    
+    //Make sure it isn't already declared in the app delegate (possible redefinition of func error)
+    func applicationDidBecomeActive(application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+    }
+
+
+    }
---
+++ Acquere Swift 2.0 FBSDK Error/ParseStarterProject-Swift/ParseStarterProject/EmailSignUpViewController.swift
@@ -0,0 +1,133 @@
+//
+//  EmailSignUpViewController.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Rahul Shrestha on 9/18/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+//import SwiftValidator
+import Parse
+import DTIActivityIndicator
+
+
+class EmailSignUpViewController: UIViewController{
+    @IBOutlet weak var signUpButton: UIButton!
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signUpActivityIndicator: UIActivityIndicatorView!
+    //let validator = Validator()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        decorateButton(signUpButton, color: UIColor(red: 0.114, green: 0.914, blue: 0.714, alpha: 1))
+        signUpActivityIndicator.hidden=true
+        
+        
+    }
+    
+    private func decorateButton(button: UIButton, color: UIColor) {
+        
+        button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        button.layer.borderColor = color.CGColor
+        button.backgroundColor = color
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 7
+    }
+    
+    @IBAction func signUpButtonTapped(sender: AnyObject) {
+        
+        let passwordString:String = passwordTextField.text!
+        let passwordCount = passwordString.characters.count
+        
+        if firstNameTextField.text=="" || lastNameTextField.text=="" || emailAddressTextField.text=="" || passwordTextField.text=="" || confirmPasswordTextField.text==""{
+            let blankFieldAlert = UIAlertController(title: "Empty Form Field Error", message: "All the Entries are required.", preferredStyle: UIAlertControllerStyle.Alert)
+            blankFieldAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(blankFieldAlert, animated: true, completion: nil);
+        }
+        else if !isValidEmail(emailAddressTextField.text!){
+            let emailErrorAlert = UIAlertController(title: "Incorrect Email Format", message: "Please Enter a Valid Email Address.", preferredStyle: UIAlertControllerStyle.Alert)
+            emailErrorAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(emailErrorAlert, animated: true, completion: nil);
+        }
+        else if passwordCount < 6 {
+            let passwordLengthErrorAlert = UIAlertController(title: "Weak Password", message: "Password must be greater than 5 characters. Try Again?", preferredStyle: UIAlertControllerStyle.Alert)
+            passwordLengthErrorAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(passwordLengthErrorAlert, animated: true, completion: nil);
+        }
+        else if passwordTextField.text != confirmPasswordTextField.text{
+            let passwordErrorAlert = UIAlertController(title: "Incorrect Password Combination", message: "Passwords do not match. Try Again?", preferredStyle: UIAlertControllerStyle.Alert)
+            passwordErrorAlert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(passwordErrorAlert, animated: true, completion: nil);
+        }
+        else{
+            let bounds = UIScreen.mainScreen().bounds
+            let myActivityIndicatorView: DTIActivityIndicatorView = DTIActivityIndicatorView(frame: CGRect(x:bounds.size.width/2-60, y:bounds.size.height/2-10, width:120.0, height:120.0))
+            self.view.addSubview(myActivityIndicatorView)
+            myActivityIndicatorView.indicatorColor = UIColor(red: 0.114, green: 0.914, blue: 0.714, alpha: 1)
+            
+            myActivityIndicatorView.indicatorStyle = DTIIndicatorStyle.convInv(.chasingDots)
+            myActivityIndicatorView.startActivity()
+
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            view?.backgroundColor = UIColor(white: 1, alpha: 0.8)
+            var user = PFUser()
+            user.email = emailAddressTextField.text
+            user.password = passwordTextField.text
+            user.username = emailAddressTextField.text
+            user["firstName"] = firstNameTextField.text
+            user["lastName"] = lastNameTextField.text
+            user.signUpInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
+                if error != nil{
+                    let error = UIAlertController(title: "Multiple Account", message: "Account Alreday Registered with this Email.", preferredStyle: UIAlertControllerStyle.Alert)
+                    error.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action: UIAlertAction!) -> () in
+                        self.performSegueWithIdentifier("signUpByEmailToMainSignUp", sender: nil)
+                    }))
+                    self.presentViewController(error, animated: true, completion: nil);
+                    self.view?.backgroundColor = UIColor(red: 0.957, green: 0.263, blue: 0.212, alpha: 1)
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    myActivityIndicatorView.stopActivity()
+                }
+                else{
+                    let activitySeconds = 5.0
+                    let activityDelay = activitySeconds * Double(NSEC_PER_SEC)  // nanoseconds per seconds
+                    let activityDispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(activityDelay))
+                    // present this after //seconds//
+                    dispatch_after(activityDispatchTime, dispatch_get_main_queue(), {
+                        myActivityIndicatorView.stopActivity()
+                        UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                        self.view?.backgroundColor = UIColor(red: 0.302, green: 0.714, blue: 0.675, alpha: 1)
+                        let success = UIAlertController(title: "Congratulations!", message: "Please Check your email for account verification.", preferredStyle: UIAlertControllerStyle.Alert)
+                        success.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action: UIAlertAction!) -> () in
+                            self.performSegueWithIdentifier("signUpByEmailToLogIn", sender: nil)
+                            PFUser.logOut()
+                        }))
+                        self.presentViewController(success, animated: true, completion: nil);
+                        
+                    })
+                    
+                  
+                }
+            })
+            
+        }
+        
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let range = testStr.rangeOfString(emailRegEx, options:.RegularExpressionSearch)
+        let result = range != nil ? true : false
+        return result
+        
+    }
+    
+    
+}
