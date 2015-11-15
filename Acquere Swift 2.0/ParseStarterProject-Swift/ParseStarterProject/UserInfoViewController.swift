0546467e3a286f1b34a213ea0f56b90c01acd04d
RAHUL SHRESTHA <rahul.shrestha@selu.edu>
2015-11-14 22:57:06 -0600

    fixed the previous issue, and the user info page is kind of complete now.

--- Acquere Swift 2.0/ParseStarterProject-Swift/ParseStarterProject/UserInfoViewController.swift
+++ Acquere Swift 2.0/ParseStarterProject-Swift/ParseStarterProject/UserInfoViewController.swift
@@ -15,6 +15,7 @@ class UserInfoViewController: UIViewController,UINavigationControllerDelegate,UI
     @IBOutlet weak var numberOfFollowers: UILabel!
     @IBOutlet weak var followingNumber: UILabel!
     @IBOutlet weak var userProfileImage: UIImageView!
+    @IBOutlet weak var userEmailAddress: UILabel!
     
     @IBOutlet weak var showUsername: UIButton!
     var currentUser = PFUser.currentUser()
@@ -22,12 +23,64 @@ class UserInfoViewController: UIViewController,UINavigationControllerDelegate,UI
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        decorateButton(showRealName, color: UIColor(red: 0.506, green: 0.831, blue: 0.98, alpha: 1))
+        decorateLabel(showRealName, color: UIColor(red: 0.506, green: 0.831, blue: 0.98, alpha: 1))
+        userEmailAddress.text = currentUser!["email"] as! String
         
         showRealName.text = " "+(currentUser!["firstName"] as? String)! + " " +
             (currentUser!["lastName"] as? String)!+"  "
+    
+        if(currentUser!["hasHashTagUsername"] as! Bool  == false){
+            showUsername.setTitle("Add @UserName..", forState: .Normal)
+        } else{ // TODO : provide the username here
+            decorateButton(showUsername, color: UIColor(red: 0.957, green: 0.263, blue: 0.212, alpha: 1))
+            var finalUsername = "@" + (currentUser!["hashtagUsername"] as! String)
+            showUsername.setTitle(finalUsername, forState: .Normal)
+        }
     }
 
+    override func viewWillAppear(animated: Bool) {
+        var finalUsername = "@" + (currentUser!["hashtagUsername"] as! String)
+        showUsername.setTitle(finalUsername, forState: .Normal)    }
+    
+    @IBAction func addUsername(sender: AnyObject) {
+        
+        // button action when there is no username
+        if(currentUser!["hasHashTagUsername"] as! Bool  == false){
+            
+            currentUser!["hasHashTagUsername"]  = true
+            currentUser?.saveInBackground()
+            
+            var alert = UIAlertController(title: "Add a unique username!!!", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addTextFieldWithConfigurationHandler(configurationTextField)
+            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel,handler: { (action: UIAlertAction!) in
+                print("Handle Cancel Logic here")
+            }))
+            alert.addAction(UIAlertAction(title: "Done", style: UIAlertActionStyle.Default, handler:{ (UIAlertAction)in
+                print("Done !!")
+                self.currentUser!["hashtagUsername"] =  (self.tField.text)
+                self.currentUser?.saveInBackground()
+                print("Item : \(self.tField.text)")
+                self.viewWillAppear(true)
+            }))
+            self.presentViewController(alert, animated: true, completion: {
+                print("completion block")
+            })
+            
+        } else{ // button action when the user name already exists
+            print("Alreday have a user name!")
+        }
+    }
+    
+    var tField: UITextField!
+    
+    func configurationTextField(textField: UITextField!)
+    {
+        print("generating the TextField")
+        textField.placeholder = "Enter Username"
+        tField = textField
+    }
+    
     @IBAction func goBack(sender: AnyObject) {
         self.performSegueWithIdentifier("backToTabbedViewFromUserInfo", sender: self)
     }
@@ -63,12 +116,22 @@ class UserInfoViewController: UIViewController,UINavigationControllerDelegate,UI
         self.presentViewController(image, animated: true, completion: nil)
     }
     
-    private func decorateButton(label: UILabel, color: UIColor) {
+    private func decorateLabel(label: UILabel, color: UIColor) {
         
         label.layer.borderColor = color.CGColor
         label.backgroundColor = color
         label.layer.borderWidth = 2
         label.layer.cornerRadius = 7
     }
+    
+    private func decorateButton(button: UIButton, color: UIColor) {
+        
+        button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        button.layer.borderColor = color.CGColor
+        button.backgroundColor = color
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 7
+    }
+    
 
 }
